28c10b4aa318dfce6a5fb345b14623f4d17fd3a7
zahranfakiraa <zahran.fakira04@gmail.com>
2024-08-06 15:24:34 +0700

    service

---
+++ src/main/java/org/ocs/billing/service/Price_Service.java
@@ -0,0 +1,12 @@
+package org.ocs.billing.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Price_Service {
+
+    // @Autowired
+    // private Price price;
+
+
+}
