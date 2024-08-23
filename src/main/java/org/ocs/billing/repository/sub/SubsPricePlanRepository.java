83c2f84dc21083e8e2845dedbfbff9b697e9a146
AzkaDevanda <azkadevanda8@gmail.com>
2024-08-23 18:27:48 +0700

    update repo

--- src/main/java/org/ocs/billing/repository/sub/SubsPricePlanRepository.java
+++ src/main/java/org/ocs/billing/repository/sub/SubsPricePlanRepository.java
@@ -2,12 +2,13 @@ package org.ocs.billing.repository.sub;
 
 
 
+import org.ocs.billing.entity.pPrice.PpricePlan;
 import org.ocs.billing.entity.sub.SubsPricePlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubsPricePlanRepository extends JpaRepository<SubsPricePlan, Long> {
+public interface SubsPricePlanRepository extends JpaRepository<SubsPricePlan, PpricePlan> {
 
 
 }
