8e26f2b0b7be970c7df015e60f696586afda61f6
zahranfakiraa <zahran.fakira04@gmail.com>
2024-08-26 17:44:57 +0700

    remove annotation id from PK

--- src/main/java/org/ocs/billing/entity/prod/ProdSpecRelaPK.java
+++ src/main/java/org/ocs/billing/entity/prod/ProdSpecRelaPK.java
@@ -12,11 +12,9 @@ public class ProdSpecRelaPK implements Serializable{
     //default serial version id, required for serializable classes.
 	private static final long serialVersionUID = 1L;
 
-	@Id
 	@Column(name="src_prod_spec_id", unique=true, nullable=false, precision=6)
 	private long srcProdSpecId;
 
-	@Id
 	@Column(name="obj_prod_spec_id", unique=true, nullable=false, precision=6)
 	private long objProdSpecId;
 }
